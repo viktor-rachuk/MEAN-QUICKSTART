d77f403be738fb215a268d8ebe43c552cae1720e
root <de.nimitz@yandex.com>
2017-07-19 11:49:49 -0700

    20th July

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/src/app/components/dashboard/dashboard.component';
+import { MenuComponent } from './components/src/app/components/menu/menu.component';
+import { LoginComponent } from './components/src/app/components/login/login.component';
+import { RegisterComponent } from './components/src/app/components/register/register.component';
+import { PagenotfoundComponent } from './components/src/app/components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MenuComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule
